d4ab3bcce65853a69aa85a62e824645fdc5d22b5
Diana Bombuškárová <diana@Mac.lan>
2025-11-24 12:22:09 +0200

    spacing in between of lines

--- client/src/components/forms/ContactForm.js
+++ client/src/components/forms/ContactForm.js
@@ -130,7 +130,7 @@ const ContactForm = ({ title = 'Napíšte nám', className = '' }) => {
         <button
           type="submit"
           disabled={isSubmitting}
-          className="w-full !mt-6 text-gray-300 border border-gray-400 bg-transparent hover:text-white hover:border-white px-8 py-2 rounded-lg font-medium transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+          className="w-full !mt-2 text-gray-300 border border-gray-400 bg-transparent hover:text-white hover:border-white px-8 py-2 rounded-lg font-medium transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
         >
           {isSubmitting ? 'Odosielam...' : 'Odoslať správu'}
         </button>
